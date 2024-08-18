9c66f1a1a452e575d8dbe00e0b7ef33ad7ebde61
Hakan Tek <hakan.tek@stud.th-owl.de>
2024-08-18 20:50:38 +0200

    feat: add HbarFaucetService for HBAR transfers

---
+++ src/application/services/hbar-faucet-service.js
@@ -0,0 +1,24 @@
+const { Client, PrivateKey, Hbar, TransferTransaction, TransactionId } = require('@hashgraph/sdk');
+
+class HbarFaucetService {
+    constructor(faucetPrivateKey, faucetAccountId, network) {
+        this.client = Client.forNetwork(network);
+        this.client.setOperator(faucetAccountId, faucetPrivateKey);
+    }
+
+    async transferHbar(toAccountId, amount) {
+        const transaction = new TransferTransaction()
+            .addHbarTransfer(this.client.operatorAccountId, new Hbar(-amount))
+            .addHbarTransfer(toAccountId, new Hbar(amount))
+            .setTransactionId(TransactionId.generate(this.client.operatorAccountId))
+            .freezeWith(this.client);
+
+        const signTx = await transaction.sign(this.client.operatorPrivateKey);
+        const txResponse = await signTx.execute(this.client);
+        const receipt = await txResponse.getReceipt(this.client);
+
+        return receipt.status;
+    }
+}
+
+module.exports = HbarFaucetService;
