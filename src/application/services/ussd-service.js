e14b7e59619134c45c759f455061703862336b03
Kyler Nguyen <tkien2703@gmail.com>
2024-08-21 00:25:21 +0700

    chore: change secretFilePath to /etc/secrets

--- src/application/services/ussd-service.js
+++ src/application/services/ussd-service.js
@@ -2,7 +2,7 @@
 
 // Firebase init
 const admin = require('firebase-admin');
-const serviceAccount = require('../../../serviceAccountKey.json');
+const serviceAccount = require('/etc/secrets/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
