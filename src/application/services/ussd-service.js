10e9dfae3bec971b1a318ad8c1f5e40034316f15
Hakan Tek <hakan.tek@stud.th-owl.de>
2024-08-20 17:18:05 +0200

    Revert "fix: update path to service account key"
    This reverts commit 8f06bd1706b30b5f846d19ccbbfa7e3b186e4691.

--- src/application/services/ussd-service.js
+++ src/application/services/ussd-service.js
@@ -3,7 +3,7 @@
 // Firebase init
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const serviceAccount = require('../../../etc/secrets/serviceAccountKey.json');
+const serviceAccount = require('../config/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
